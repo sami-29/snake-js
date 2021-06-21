b8e37165e7c895284e219f719c418bdd2dc6331e
sami ben <annabisami01@gmail.com>
2021-06-21 14:33:39 +0100

    swipes

--- main.js
+++ main.js
@@ -57,6 +57,55 @@ function move() {
         let col = Math.floor(snake[1][0]/15)
         snake[1][0] = adjustToCol(snake[1][0] + 1,col)
     }
+    document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+        	snake[1].push(adjustToCol(snake[1][0] + 1 * snake[0]))
+            /* left swipe */ 
+        } else {
+        	snake[1].push(adjustToCol(snake[1][0] - 1 * snake[0]))
+            /* right swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+            snake[1].push(adjustPos(snake[1][0] + 15 * snake[0]))
+        } else { 
+            /* down swipe */
+            snake[1].push(adjustPos(snake[1][0] + 15 * snake[0]))
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
     
 }
 
