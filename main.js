ba00ab26cf966cd21bb3c3d260c2c7e0b4b914a0
sami ben <annabisami01@gmail.com>
2021-06-21 14:38:55 +0100

    swipesV2

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 const container = document.querySelector('#container')
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches || evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};
+
+function handleTouchMove(evt) {
+	if ( ! xDown || ! yDown ) {
+       	return;
+};
+
+var xUp = evt.touches[0].clientX;                                    
+var yUp = evt.touches[0].clientY;
+
+var xDiff = xDown - xUp;
+var yDiff = yDown - yUp;
 
 let ELEMENTS = null
 const makedivs = (n) => {
@@ -57,33 +82,8 @@ function move() {
         let col = Math.floor(snake[1][0]/15)
         snake[1][0] = adjustToCol(snake[1][0] + 1,col)
     }
-    document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
+                                                
 
-var xDown = null;                                                        
-var yDown = null;
-
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
-
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
-
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
 
     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
         if ( xDiff > 0 ) {
